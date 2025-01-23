302d3b9829f32c9358dc4f714f154d8886932fe6
Arthur Zall <zallarthur@gmail.com>
2025-01-23 19:59:06 -0300

    estruturação e estilização da sessão home

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+//Visualização do Menu
+
 let navMenu = document.getElementById('nav-menu');
 let navToggle = document.getElementById('nav-toggle');
 let navClose = document.getElementById('nav-close');
@@ -23,3 +25,13 @@ let linkAction = () => {
 navLink.forEach(function(n) {
     n.addEventListener('click', linkAction);
 });
+
+//Efeito de sombra durante o scroll
+
+let shadowHeader = () => {
+    let header = document.getElementById('header');
+
+    this.scrollY >= 50 ? header.classList.add('shadow-header') 
+                       : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader);
